51ad241d8572dcb01d3518440d5a47e7b9711311
Eric Terrisse <eterrissebordas@gmail.com>
2024-06-28 01:25:28 +0200

    last changes

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import React from "react";
-import houseImg from "@/assets/house.webp";
+import houseImg from "@/assets/house.png";
 import { FaHeart, FaFilePdf } from "react-icons/fa";
 import { CiShare2 } from "react-icons/ci";
+import { IoIosLink } from "react-icons/io";
 
 interface Props {}
 
@@ -10,42 +11,47 @@ const Main = (props: Props) => {
 	return (
 		<div className="flex justify-between max-w-[90rem] m-auto py-10">
 			<div className="w-[48%] flex flex-col gap-5">
-				<h2 className="text-xl font-semibold tracking-wider uppercase">
-					Habitatge analitzat:
+				<h2 className="text-xl font-semibold tracking-wider uppercase flex gap-2 items-center">
+					Habitatge analitzat:{" "}
+					<a
+						href="https://www.idealista.com/inmueble/102987023/"
+						target="_blank"
+					>
+						<IoIosLink size={30} />
+					</a>
 				</h2>
 				<Image
 					src={houseImg}
 					alt="house"
-					className="drop-shadow-lg rounded-xl"
+					className="drop-shadow-lg rounded-xl relative"
 				/>
 				<div className="flex flex-col gap-2">
 					<div>
 						<p className="text-lg font-semibold text-pink-500">
-							Pis al carrer Bosch i Gimpera, Sarrià, Barcelona
+							Piso en venta en calle de la Mare de Déu dels Desemparats
 						</p>
-						<p className="font-semibold text-xl">1.445.000€</p>
+						<p className="font-semibold text-xl">400.000€</p>
 						<div className="flex gap-5">
-							<p>3 hab.</p>
-							<p>232 m²</p>
-							<p>exterior amb ascensor</p>
+							<p>70 m²</p>
+							<p>2 hab.</p>
+							<p>Entreplanta exterior con ascensor</p>
 						</div>
 					</div>
 					<p className="text-sm text-justify">
-						Piso alto en venta, único en la zona de 232 m2 útiles terraza de 12
-						m2 en Bosch i Gimpera (Pedralbes). Amplio Salón-comedor de 70 m2
-						distribuido en varios ambientes con salida a terraza. Cocina-office
-						con lavadero y zona de servicio. Actualmente tiene 1 habitacion en
-						suite con vestidor y baño con bañera y 2 habitaciones más con 2
-						baños. En el caso de hacerse una reforma integral se podrían sacar 5
-						habitaciones en la vivienda. El piso totalmente exterior, con mucha
-						luz y vistas despejadas a Zona ajardinada. Situado en zona exclusiva
-						y muy tranquila, al lado del Club de Tenis Barcelona y de las
-						mejores escuelas internacionales. Finca con servicio de conserjería
-						y vigilancia nocturna 24/7. Dos plazas parkings y trastero. Caja de
-						seguridad. Parqué en todo el piso. DEBIDO A SU ESTRUCTURA DE
-						PILARES, y ausencia de paredes maestras tiene total libertad de
-						reforma, (incluyendo tipo Loft). No dude en contactar para ampliar
-						información o agendar una visita.
+						PISO CON BALCÓN EN EL CORAZÓN DE GRACIA Acogedor piso ubicado en el
+						corazón de Gràcia. Cerca de la plaza de la Revolución y de los Cines
+						Verdi. La ubicación es perfecta gracias a la amplia oferta de bares,
+						restaurantes y cultura. Las calles del barrio mantienen el
+						pintoresco aire de pueblo y al mismo tiempo conectado al centro de
+						Barcelona. Es una de las plazas con más sol en Gracia. Esta
+						propiedad dispone de dos apartamentos a los que se accede mediante
+						un hall de distribución central y es ideal para dos parejas o una
+						madre y una hija. Cada apartamento está compuesto por zona de día
+						con salón comedor y cocina, zona de noche con dormitorio principal y
+						baño completo. Se puede acceder al piso por ascensor o escalera.
+						¡Oportunidad de inversión! Piso distribuido en dos apartamentos
+						independientes cada uno con su salón, cocina, baño completo y
+						dormitorio. Totalmente amueblado y equipado.
 					</p>
 				</div>
 			</div>
@@ -57,65 +63,53 @@ const Main = (props: Props) => {
 					<li>Lavabos</li>
 					<ul className="list-disc ml-10 flex flex-col gap-3">
 						<li className="flex justify-between">
-							<p>Pica</p>
-							<p>800€</p>
+							<p>Pica (2)</p>
+							<p>1.400 €</p>
+						</li>
+						<li className="flex justify-between">
+							<p>Rajoles</p>
+							<p>2.200 €</p>
 						</li>
 						<li className="flex justify-between">
 							<p>Dutxa</p>
-							<p>1000€</p>
-						</li>
-						<li className="flex justify-between">
-							<p>Rejoles</p>
-							<p>12000€</p>
+							<p>2.000 €</p>
 						</li>
 					</ul>
 					<li>Cuina</li>
 					<ul className="list-disc ml-10 flex flex-col gap-3">
 						<li className="flex justify-between">
-							<p>Taulell</p>
-							<p>800€</p>
-						</li>
-						<li className="flex justify-between">
-							<p>Terra</p>
-							<p>1000€</p>
-						</li>
-						<li className="flex justify-between">
 							<p>Armaris</p>
-							<p>5000€</p>
+							<p>3.600 €</p>
+						</li>
+						<li className="flex justify-between">
+							<p>Taulell</p>
+							<p>2.500 €</p>
 						</li>
 					</ul>
 					<li>Interior</li>
 					<ul className="list-disc ml-10 flex flex-col gap-3">
 						<li className="flex justify-between">
-							<p>Finestres</p>
-							<p>8000€</p>
-						</li>
-						<li className="flex justify-between">
-							<p>Pintura</p>
-							<p>2000€</p>
-						</li>
-						<li className="flex justify-between">
-							<p>Armaris</p>
-							<p>5000€</p>
+							<p>Terra (60%)</p>
+							<p>4.680 €</p>
 						</li>
 					</ul>
 					<span className="w-full h-[1px] bg-black" />
 					<div className="flex justify-between">
 						<p>Total despeses</p>
-						<p>-50.000€</p>
+						<p>-16.380 €</p>
 					</div>
 					<div className="flex justify-between">
 						<p>Preu inicial</p>
-						<p>1.000.000€</p>
+						<p>400.000 €</p>
 					</div>
 					<div className="flex justify-between">
-						<p>Preu post-reforma (7%)</p>
-						<p>1.070.000€</p>
+						<p>Preu post-reforma (10%)</p>
+						<p>440.000 €</p>
 					</div>
 					<span className="w-full h-[1px] bg-black" />
 					<div className="flex justify-between font-bold">
 						<p>Benefici</p>
-						<p>20.000€</p>
+						<p>23.620 €</p>
 					</div>
 				</ul>
 				<div className="flex justify-between">
